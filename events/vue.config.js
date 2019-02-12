1c13dc9d37ef8776b67752ac981ccf9131b6e0ac
Basil K Y <basilky145@gmail.com>
2019-02-12 15:26:36 +0530

    built successfully

--- events/vue.config.js
+++ events/vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     {
         config.optimization.splitChunks(false)
     },
+    filenameHashing: false,
     runtimeCompiler: true,
     configureWebpack: {
         devServer: {
